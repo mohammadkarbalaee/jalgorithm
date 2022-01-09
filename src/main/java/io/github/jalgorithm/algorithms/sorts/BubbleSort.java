cf6237e0184f63a5fb706d5a0328356e328fb091
hreshi <hkale746@gmail.com>
2022-01-09 13:06:36 +0530

    Bubble Sort Enhancement : Range sort method added

--- src/main/java/io/github/jalgorithm/algorithms/sorts/BubbleSort.java
+++ src/main/java/io/github/jalgorithm/algorithms/sorts/BubbleSort.java
@@ -43,8 +43,22 @@ public class BubbleSort<T extends Comparable<T>> extends SortAlgorithm{
    * @apiNote bubble sort is so efficient for data which is already sorted and just has some minor flaws.
    */
   public void sort() {
-    for (int prevSwap = 0, end = data.size() - 1; end != 0; end = prevSwap) {
-      prevSwap = 0;
+    sort(0, data.size());
+  }
+
+  /**
+   * this method sorts a part of the array indicated by left and right index
+   * @param left is the starting of the partition to sort. It is inclusive
+   * @param right is the end of the partition to sort. It is exclusive
+   * it doesn't return anything.To access the sorted data, call the getData method.
+   * @apiNote bubble sort is so efficient for data which is already sorted and just has some minor flaws.
+   */
+  public void sort(int left, int right) {
+    if(!checkBounds(left, right)) {
+      throw new IndexOutOfBoundsException("left:" + left + " right:" + right + " size:" + data.size());
+    }
+    for (int prevSwap = left, end = right - 1; end != 0; end = prevSwap) {
+      prevSwap = left;
       for (int j = 1; j <= end; j++) {
         int comparisonResult = this.data.get(j).compareTo(this.data.get(j - 1));
         if (comparisonResult < 0) {
@@ -55,6 +69,13 @@ public class BubbleSort<T extends Comparable<T>> extends SortAlgorithm{
     }
   }
 
+  /**
+   * this method checks bounds for left and right indexes given to sort method
+   * @return true if bounds are correct else false.
+   */
+  private boolean checkBounds(int left, int right) {
+    return left < right && left >= 0 && right <= data.size();
+  }
   /**
    *
    * @return data or sorted data. depends on whether you have called sort method before calling it or not
