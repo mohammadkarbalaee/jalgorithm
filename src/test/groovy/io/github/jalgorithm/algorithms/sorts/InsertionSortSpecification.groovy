e5d5d6a5a27b2d0e5a1d4baf6c1bb44fea390bca
mayur-ravindram <mayurravindram@gmail.com>
2022-01-09 14:55:42 +0530

    Adding test cases for InsertionSort
    https://github.com/muhammadkarbalaee/jalgorithm/issues/43
    
    Test is in progress, committing the skeleton

---
+++ src/test/groovy/io/github/jalgorithm/algorithms/sorts/InsertionSortSpecification.groovy
@@ -0,0 +1,50 @@
+package io.github.jalgorithm.algorithms.sorts
+
+import static org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * 
+ * @author mayur-ravindra
+ * ** THIS SPECIFICATION IS IN PROGRESS **
+ *
+ */
+class InsertionSortSpecification extends Specification {
+
+	@Subject
+	private InsertionSort algorithm;
+	
+	def "sorting integers"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+
+	def "sorting floats"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+
+	def "sorting doubles"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+
+	def "sorting strings"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+
+	def "sorting longs"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+	
+	def "sorting duplicates"() {
+		expect: "test is in progress"
+		1 == 1
+	}
+}
