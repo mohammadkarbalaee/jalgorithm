05fb10ec829588ebeb10483dfb3e5cbc49548944
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-01-11 13:58:33 +0330

    implemented queue data structure

--- src/main/java/io/github/jalgorithm/datastructures/Queue.java
+++ src/main/java/io/github/jalgorithm/datastructures/Queue.java
@@ -1,10 +1,12 @@
 package io.github.jalgorithm.datastructures;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 /**
- *
- * @param <T>
+ *Queue class delivers the functionality of Queue data structure.
+ * @author Muhammad Karbalaee
+ * @param <T> any type is possible
  */
 public class Queue<T>{
   private final T[] array;
@@ -12,13 +14,37 @@ public class Queue<T>{
   private int tail;
   private int head;
 
-  Queue(int queueLength){
+  /**
+   * constructor for Queue class
+   * @param queueLength the length of the queue that you want to make
+   */
+  public Queue(int queueLength){
     this.array = (T[]) new Object[queueLength];
     this.head = 0;
     this.tail = 0;
     this.queueLength = 0;
   }
 
+  /**
+   *
+   * @return an array containing all the data in the queue
+   */
+  public T[] getData() {
+    return this.array;
+  }
+
+  /**
+   *
+   * @return to get the length of queue
+   */
+  public int getQueueLength() {
+    return queueLength;
+  }
+
+  /**
+   * adds an element to the end of the queue
+   * @param newElement the new element to add to the queue
+   */
   public void enqueue(T newElement){
     if (isQueueFull()){
       throw new OutOfMemoryError("queue is already full");
@@ -33,6 +59,10 @@ public class Queue<T>{
     }
   }
 
+  /**
+   * deletes an element from the start of the queue.
+   * @return the first element of queue
+   */
   public T dequeue(){
     if (isQueueEmpty()){
       throw new NoSuchElementException("queue is already empty");
@@ -48,10 +78,18 @@ public class Queue<T>{
     }
   }
 
+  /**
+   *
+   * @return true of queue has no elements in it
+   */
   public boolean isQueueEmpty(){
     return queueLength == 0;
   }
 
+  /**
+   *
+   * @return true if queue has the most number of elements that is can store.
+   */
   public boolean isQueueFull(){
     return this.array.length == queueLength;
   }
