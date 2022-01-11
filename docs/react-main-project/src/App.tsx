5c761effeb9514ee6bc5249c6f73434f5576ffb2
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-01-11 13:11:07 +0330

    finished making the temporary website

--- docs/react-main-project/src/App.tsx
+++ docs/react-main-project/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './jalgorithm.jpg';
-import './App.css';
+import './App.scss';
 import Draggable from 'react-draggable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Draggable>
-          <div>
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-        </Draggable>
+        <div id={'logo-box'}>
+          <Draggable>
+            <div>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </Draggable>
+        </div>
         drag me!🙄
         <p>
           <code>Jalgorithm</code>
           <br/>
-          the DSA library to beat them all.
+          We offer what Java developers need and deserve💪
           <br/>
-          coming soon....
+          v2.0.0 coming soon....
         </p>
         <a
           className="App-link"
@@ -26,6 +28,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <img src={'https://www.vectorlogo.zone/logos/github/github-ar21.svg'} width={200} alt={'github'}/>
+          <br/>
           Jalgorithm's github repo
         </a>
       </header>
