67656ecece809d893fc64973d37fd483b0b983f3
mayur-ravindram <mayurravindram@gmail.com>
2022-01-22 20:24:28 +0530

    Code changes for issue #43
    https://github.com/muhammadkarbalaee/jalgorithm/issues/43

--- src/test/groovy/io/github/jalgorithm/algorithms/sorts/InsertionSortSpecification.groovy
+++ src/test/groovy/io/github/jalgorithm/algorithms/sorts/InsertionSortSpecification.groovy
@@ -1,50 +1,106 @@
 package io.github.jalgorithm.algorithms.sorts
 
-import static org.junit.jupiter.api.Assertions.*
-
-import org.junit.jupiter.api.Test
-
 import spock.lang.Specification
 import spock.lang.Subject
 
 /**
  * 
  * @author mayur-ravindra
- * ** THIS SPECIFICATION IS IN PROGRESS **
  *
  */
 class InsertionSortSpecification extends Specification {
 
 	@Subject
 	private InsertionSort algorithm;
-	
+
 	def "sorting integers"() {
-		expect: "test is in progress"
-		1 == 1
+		given: "unsorted list of integers"
+		def data = [78, 2, 33, 6, 0, -11]
+		algorithm = new InsertionSort(data)
+
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
 
 	def "sorting floats"() {
-		expect: "test is in progress"
-		1 == 1
+		given: "unsorted list of floating point numbers"
+		def data = [
+			7.8f,
+			2.33f,
+			-33.4f,
+			6.0f,
+			0.0f,
+			-11.23f
+		]
+		algorithm = new InsertionSort(data)
+
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
 
 	def "sorting doubles"() {
-		expect: "test is in progress"
-		1 == 1
+		given: "unsorted list of doubles"
+		def data = [
+			78.2,
+			2.4,
+			33.5,
+			6.3,
+			0.6,
+			-11.4
+		]
+		algorithm = new InsertionSort(data)
+
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
 
 	def "sorting strings"() {
-		expect: "test is in progress"
-		1 == 1
+		given: "unsorted list of strings"
+		def data = ["z", "p", "a", "A"]
+		algorithm = new InsertionSort(data)
+
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
 
 	def "sorting longs"() {
-		expect: "test is in progress"
-		1 == 1
+		given: "unsorted list of longs"
+		def data = ["z", "p", "a", "A"]
+		algorithm = new InsertionSort(data)
+
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
-	
+
 	def "sorting duplicates"() {
-		expect: "test is in progress"
-		1 == 1
+		given:
+		def data = ["a", "a", "A"]
+		algorithm = new InsertionSort(data)
+		when: "sorted"
+		algorithm.sort()
+
+		then: "assert that the list is sorted"
+		Arrays.sort(data)
+		algorithm.getData() == data
 	}
 }
